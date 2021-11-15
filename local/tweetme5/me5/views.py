14dcaac73c691a5c2c85e5dfbf4f70b30f4dbdb9
BabyTigerChaseDream <BabyTigerChaseDream@github.com>
2021-11-15 08:51:41 +0800

    me5 beofre node.js

--- local/tweetme5/me5/views.py
+++ local/tweetme5/me5/views.py
@@ -10,8 +10,11 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from .forms import TweetForm
 from .models import Tweet
-from .serializers import TweetSerializer, TweetActionSerializer
-
+from .serializers import (
+    TweetSerializer, 
+    TweetActionSerializer,
+    TweetCreateSerializer
+)
 ALLOWED_HOSTS = settings.ALLOWED_HOSTS
 
 # Create your views here.
@@ -61,23 +64,31 @@ def tweet_action_view(request, *args, **kwargs):
     id is required.
     Action options are: like, unlike, retweet
     '''
-    serializer = TweetActionSerializer(request.POST)
+    serializer = TweetActionSerializer(data=request.data)
     if serializer.is_valid(raise_exception=True):
         data = serializer.validated_data
         tweet_id = data.get("id")
         action = data.get("action")
+        content = data.get("content")
         qs = Tweet.objects.filter(id=tweet_id)
         if not qs.exists():
             return Response({}, status=404)
         obj = qs.first()
         if action == "like":
             obj.likes.add(request.user)
+            serializer = TweetSerializer(obj)
+            return Response(serializer.data, status=200)
         elif action == "unlike":
             obj.likes.remove(request.user)
         elif action == "retweet":
-            # this is todo
-            pass
-    return Response({"message": "Tweet removed"}, status=200)
+            new_tweet = Tweet.objects.create(
+                    user=request.user, 
+                    parent=obj,
+                    content=content,
+                    )
+            serializer = TweetSerializer(new_tweet)
+            return Response(serializer.data, status=200)
+    return Response({}, status=200)
 
 
 @api_view(['GET'])
